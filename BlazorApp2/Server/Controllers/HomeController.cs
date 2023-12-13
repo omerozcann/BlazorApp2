ef4776c69517a8b98b6f73a66c3569441b5e4c9b
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-13 17:43:19 +0300

    Proje dosyası ekle.

---
+++ BlazorApp2/Server/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp2.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [Route("kaydet")]
+        [HttpPost]
+        public void kaydet(kullanici k)
+        {
+            StreamWriter sw = new StreamWriter("kayit.txt", true);
+            sw.WriteLine(k.adsoyad);
+            sw.WriteLine(k.numara);
+            sw.Close();
+        }
+    }     
+}
---
+++ BlazorApp2/Shared/kullanici.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class kullanici
+    {
+        public String adsoyad { get; set; }
+        public String numara { get; set; }
+    }
+}
